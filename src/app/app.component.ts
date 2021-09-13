dfa4bf4bc6f26adbc72d8cc824ef928c0d2ca8be
reachsanthosh <reachsanthosh0305@gmail.com>
2021-09-12 23:30:15 -0400

    adding new checkin

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { User } from './_models/User';
+import { AccountService } from './_services/account.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'The Chat App';
+  users: any 
+
+  //constructor(private http: HttpClient, private accountService: AccountService)  {}
+  constructor(private accountService: AccountService)  {}
+
+  ngOnInit(): void {
+    //this.getUsers();
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+
+    const user: User = JSON.parse(localStorage.getItem('user') as any);  
+    this.accountService.setCurrentUser(user);
+  }
+
+  /*getUsers()
+  {
+    this.http.get('https://localhost:5001/api/Users').subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error); 
+    });
+  }*/
+
+}
